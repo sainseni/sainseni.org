ffff7eef2c338e7fb9f702e16d6e7bc4384958dd
khoironi <khoironi@zero-one-group.com>
2023-07-03 19:54:18 +0800

    init

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+
+import { appRouter } from '@/server/api/root';
+import { createTRPCContext } from '@/server/api/trpc';
+
+import { env } from '@/env.mjs';
+
+// export API handler
+export default createNextApiHandler({
+    router: appRouter,
+    createContext: createTRPCContext,
+    onError:
+        env.NODE_ENV === 'development'
+            ? ({ path, error }) => {
+                  console.error(
+                      `❌ tRPC failed on ${path ?? '<no-path>'}: ${
+                          error.message
+                      }`
+                  );
+              }
+            : undefined,
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from '@/ui/ComingSoon';
+
+export default function Home() {
+    return <ComingSoon />;
+}
