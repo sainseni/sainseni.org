451082c552cfa5af9fa46af271a923c83067fb48
zekhoi <khoironidev@gmail.com>
2023-07-03 22:16:43 +0800

    chore: update site config

--- site.config.js
+++ site.config.js
@@ -2,8 +2,7 @@
 export default {
     domain: process.env.NEXTAUTH_URL || 'https://localhost:3000',
     name: 'Sainseni',
-    description:
-        'A software engineer who focus on backend development but also likes user interface. Mathematics student. Indonesian.',
+    description: 'Community',
     socials: [
         { href: 'https://github.com/sainseni', label: 'Github' },
         { href: 'https://twitter.com/sainseni', label: 'Twitter' },
@@ -16,14 +15,13 @@ export default {
     top_navlinks: [
         { href: '/', label: 'Home' },
         { href: '/about', label: 'About' },
-        { href: '/blog', label: 'Blog' },
+        { href: '/team', label: 'Team' },
         { href: '/projects', label: 'Projects' },
     ],
     bottom_navlinks: [
         { href: '/about', label: 'About' },
         { href: '/team', label: 'Team' },
-        { href: '/feedback', label: 'Feedback' },
         { href: '/contact', label: 'Contact' },
-        { href: '/policy', label: 'Privacy' },
+        { href: 'https://status.sainseni.org', label: 'Status' },
     ],
 };
