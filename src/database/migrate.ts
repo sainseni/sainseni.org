d43f22b805812589b163b104fd389520f12f14c9
Khoironi Kurnia Syah <khoironidev@gmail.com>
2024-08-25 15:34:57 +0800

    ref: rewrite to previous

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -4,18 +4,8 @@ import 'dotenv/config';
 import { database, pg } from './index';
 
 async function main() {
-    try {
-        await migrate(database, { migrationsFolder: 'drizzle' });
-
-        console.log('Migration completed');
-    } catch (error) {
-        console.error(
-            (error as Error)?.message ||
-                'An error occurred while migrating the database.',
-        );
-    } finally {
-        await pg.end();
-    }
+    await migrate(database, { migrationsFolder: 'drizzle' });
+    await pg.end();
 }
 
 main();
--- src/database/seed.ts
+++ src/database/seed.ts
@@ -6,52 +6,43 @@ import env from '@/env';
 import { database, pg } from './index';
 
 async function main() {
-    try {
-        if (env.NODE_ENV !== 'development') {
-            throw new Error('Seed is only allowed in development environment');
-        }
-
-        const [admin, user] = await database
-            .insert(table.role)
-            .values([
-                {
-                    name: 'admin',
-                },
-                {
-                    name: 'user',
-                },
-            ])
-            .onConflictDoNothing()
-            .returning();
-
-        await database
-            .insert(table.user)
-            .values([
-                {
-                    username: 'jokowi',
-                    email: 'jokowi@indonesia.go.id',
-                    accountType: 'google',
-                    role: admin.id,
-                },
-                {
-                    username: 'prabowo',
-                    email: 'prabowo@indonesia.go.id',
-                    accountType: 'github',
-                    role: user.id,
-                },
-            ])
-            .onConflictDoNothing()
-            .returning();
-
-        console.log('Seed completed');
-    } catch (error) {
-        console.error(
-            (error as Error)?.message ||
-                'An error occurred while seeding the database.',
-        );
-    } finally {
-        await pg.end();
+    if (env.NODE_ENV !== 'development') {
+        throw new Error('Seed is only allowed in development environment');
     }
+
+    const [admin, user] = await database
+        .insert(table.role)
+        .values([
+            {
+                name: 'admin',
+            },
+            {
+                name: 'user',
+            },
+        ])
+        .onConflictDoNothing()
+        .returning();
+
+    await database
+        .insert(table.user)
+        .values([
+            {
+                username: 'jokowi',
+                email: 'jokowi@indonesia.go.id',
+                accountType: 'google',
+                role: admin.id,
+            },
+            {
+                username: 'prabowo',
+                email: 'prabowo@indonesia.go.id',
+                accountType: 'github',
+                role: user.id,
+            },
+        ])
+        .onConflictDoNothing()
+        .returning();
+
+    await pg.end();
 }
 
 main();
